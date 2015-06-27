0c8913753d08ead4e6024cf37730fb74f8bef190
tobika <tobias.kausch@tobik.eu>
2015-06-27 23:43:52 +0200

    channel filter is working

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,18 @@
-$("img.lazy").show().lazyload();
+$('img.lazy').show().lazyload();
 
-$("img.lazy").lazyload({
+$('img.lazy').lazyload({
     effect : "fadeIn"
+});
+
+$( ".channelfilter" ).on( 'click', function() {
+    var channel = $(this).text();
+
+    $('.show').hide();
+
+    var selected = $('.show').filter(function() {
+
+        return $(this).data('channels').indexOf(channel) >= 0;
+    }).show();
+
+    $('img.lazy').lazyload();
 });
