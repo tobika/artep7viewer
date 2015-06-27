0f73ce987836fe3832462abb7cef745390bc5bd7
tobika <tobias.kausch@tobik.eu>
2015-06-27 16:09:41 +0200

    Change port + template

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/', function (req, res) {
     });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
 
     var host = server.address().address;
     var port = server.address().port;
