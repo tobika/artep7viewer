e648e5d16be270bc528f20fcd5c0722d0a74af48
tobika <tobias.kausch@tobik.eu>
2015-11-04 11:23:50 +0100

    Correct german categories

--- src/arteCategories.js
+++ src/arteCategories.js
@@ -10,13 +10,13 @@ module.exports.categories = {
         {code: 'JUN', name: 'Junior'},
     ],
     de : [
-        {code: 'ACT', name: 'Actu & société'},
-        {code: 'CIN', name: 'Cinéma'},
-        {code: 'FIC', name: 'Séries & fiction'},
-        {code: 'ART', name: 'Arts & spectacles classiques'},
-        {code: 'CUL', name: 'Culture pop'},
-        {code: 'DEC', name: 'Découverte'},
-        {code: 'HIS', name: 'Histoire'},
+        {code: 'ACT', name: 'Aktuelles & Gesellschaft'},
+        {code: 'CIN', name: 'Kino'},
+        {code: 'FIC', name: 'Fernsehfilme & Serien'},
+        {code: 'ART', name: 'Kunst & Kultur'},
+        {code: 'CUL', name: 'Popkultur & Alternativ'},
+        {code: 'DEC', name: 'Entdeckung'},
+        {code: 'HIS', name: 'Geschichte'},
         {code: 'JUN', name: 'Junior'},
     ]
 };
