cfc38200b7ef29c63ba5e38fb250d6136f975060
tobika <tobias.kausch@tobik.eu>
2017-03-08 23:49:45 +0100

    move to eslint 1

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
 var express = require('express'),
-    hbs = require('express-hbs'),
-    arteDB = require('./src/arteDB'),
-    app = express();
+  hbs = require('express-hbs'),
+  arteDB = require('./src/arteDB'),
+  app = express();
 
 // I am guilty of stackoverflow ;) http://stackoverflow.com/a/20429914
 function nocache(req, res, next) {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    next();
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  next();
 }
 
 app.get('/', function (req, res) {
 
-    var accept = req.acceptsLanguages(['fr', 'de']);
+  var accept = req.acceptsLanguages(['fr', 'de']);
 
-    if (accept == 'fr') {
-        res.redirect('/fr');
-    }
-    else {
-        res.redirect('/de');
-    }
+  if (accept == 'fr') {
+    res.redirect('/fr');
+  }
+  else {
+    res.redirect('/de');
+  }
 
 });
 
 app.get('/:language', nocache, function (req, res) {
 
-    if (!arteDB.dataLoaded) {
-        res.render('starting');
-        return;
-    }
+  if (!arteDB.dataLoaded) {
+    res.render('starting');
+    return;
+  }
 
-    var language = req.params.language;
+  var language = req.params.language;
 
-    if (language === 'de' || language === 'fr') {
+  if (language === 'de' || language === 'fr') {
 
-        res.render('index', {
-            title: 'Arte plus7 viewer - Unofficial',
-            shows: arteDB.tvGuideData[language],
-            channels: arteDB.channelData[language]
-        });
-    }
-    else {
-        res.send('No such language.');
-    }
+    res.render('index', {
+      title: 'Arte plus7 viewer - Unofficial',
+      shows: arteDB.tvGuideData[language],
+      channels: arteDB.channelData[language]
+    });
+  }
+  else {
+    res.send('No such language.');
+  }
 });
 
 var port = process.env.PORT || 5000;
 
 var server = app.listen(port, function () {
 
-    var host = server.address().address;
-    var port = server.address().port;
+  var host = server.address().address;
+  var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port);
 
 });
 
 app.use(express.static('public'));
 app.engine('hbs', hbs.express4({
-    partialsDir: __dirname + '/views/partials'
+  partialsDir: __dirname + '/views/partials'
 }));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
--- public/js/index.js
+++ public/js/index.js
@@ -1,84 +1,71 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    var myLazyLoad = new LazyLoad({
-        // example of options object -> see options section
-        //threshold: 500,
-        //container: document.querySelector('.container'),
-        elements_selector: "img.lazy",
-        throttle: 200,
-        //data_src: "src",
-        //data_srcset: "srcset",
-        //callback_set: function() { /* ... */ }
+  var myLazyLoad = new LazyLoad({
+    // example of options object -> see options section
+    //threshold: 500,
+    //container: document.querySelector('.container'),
+    elements_selector: 'img.lazy',
+    throttle: 200,
+    //data_src: "src",
+    //data_srcset: "srcset",
+    //callback_set: function() { /* ... */ }
+  });
+
+  $('.channelfilter').on('click', function () {
+    var channel = $(this).text();
+    var $allShows = $('.show');
+
+    if (channel === 'All') {
+      $allShows.show();
+      return;
+    }
+
+    $allShows.hide();
+
+    $allShows.filter(function () {
+      return $(this).data('channels').indexOf(channel) >= 0;
+    }).show();
+
+    myLazyLoad.handleScroll();
+  });
+
+  function getFirstShowID() {
+    return $('.show').first().data('em');
+  }
+
+  function getShowByID(id) {
+    return $('.show').filter(function () {
+      return $(this).data('em') === id;
+    });
+  }
+
+  function getShowByIndex(index) {
+    return $('.show').filter(function () {
+      return $(this).data('index') === index;
+    });
+  }
+
+  function colorNewShows() {
+    if (!Lockr.get('newestID')) {
+      Lockr.set('newestID', getFirstShowID());
+    } else {
+      getShowByID(Lockr.get('newestID')).prevAll().addClass('new');
+      Lockr.set('newestID', getFirstShowID());
+    }
+  }
+
+  $(document).ready(function () {
+    $('#channels').sticky({ topSpacing: 0 });
+    colorNewShows();
+
+    $('#container-search').hideseek({
+      ignore_accents: true,
+      highlight: true
     });
 
-    $( ".channelfilter" ).on( 'click', function() {
-
-        var channel = $(this).text(),
-            $allShows = $('.show');
-
-        if (channel === 'All') {
-            $allShows.show();
-            return;
-        }
-
-        $allShows.hide();
-
-        var selected = $allShows.filter(function() {
-
-            return $(this).data('channels').indexOf(channel) >= 0;
-        }).show();
-
-        myLazyLoad.handleScroll();
+    $('#container-search').on('_after', function () {
+      myLazyLoad.handleScroll();
     });
-
-    function getFirstShowID() {
-
-        return $('.show').first().data('em');
-    }
-
-    function getShowByID(id) {
-
-        return $('.show').filter( function() {
-            return $(this).data('em') == id;
-        });
-    }
-
-    function getShowByIndex(index) {
-
-        return $('.show').filter( function() {
-            return $(this).data('index') == index;
-        });
-    }
-
-    function colorNewShows(id) {
-
-        if (!Lockr.get('newestID')) {
-
-            Lockr.set('newestID', getFirstShowID());
-        }
-        else {
-
-            //test feature
-            //Lockr.set('newestID', '048711-008');
-
-            getShowByID(Lockr.get('newestID')).prevAll().addClass('new');
-            Lockr.set('newestID', getFirstShowID());
-        }
-    }
-
-    $(document).ready(function(){
-
-        $("#channels").sticky({topSpacing:0});
-        colorNewShows();
-
-        $('#container-search').hideseek( {
-            ignore_accents: true,
-            highlight: true
-        });
-
-        $('#container-search').on("_after", function() {
-            myLazyLoad.handleScroll();
-        });
-    });
-})();
+  });
+}());
