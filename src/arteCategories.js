703f015111f3a0a171a4d0b0de3eb9f174c22399
tobika <tobias.kausch@tobik.eu>
2017-03-09 00:18:55 +0100

    move to eslint 2 - finished

--- src/arteCategories.js
+++ src/arteCategories.js
@@ -1,22 +1,22 @@
 module.exports.categories = {
-    fr : [
-        {code: 'ACT', name: 'Actu & société'},
-        {code: 'CIN', name: 'Cinéma'},
-        {code: 'FIC', name: 'Séries & fiction'},
-        {code: 'ART', name: 'Arts & spectacles classiques'},
-        {code: 'CUL', name: 'Culture pop'},
-        {code: 'DEC', name: 'Découverte'},
-        {code: 'HIS', name: 'Histoire'},
-        {code: 'JUN', name: 'Junior'},
-    ],
-    de : [
-        {code: 'ACT', name: 'Aktuelles & Gesellschaft'},
-        {code: 'CIN', name: 'Kino'},
-        {code: 'FIC', name: 'Fernsehfilme & Serien'},
-        {code: 'ART', name: 'Kunst & Kultur'},
-        {code: 'CUL', name: 'Popkultur & Alternativ'},
-        {code: 'DEC', name: 'Entdeckung'},
-        {code: 'HIS', name: 'Geschichte'},
-        {code: 'JUN', name: 'Junior'},
-    ]
+  fr: [
+    { code: 'ACT', name: 'Actu & société' },
+    { code: 'CIN', name: 'Cinéma' },
+    { code: 'FIC', name: 'Séries & fiction' },
+    { code: 'ART', name: 'Arts & spectacles classiques' },
+    { code: 'CUL', name: 'Culture pop' },
+    { code: 'DEC', name: 'Découverte' },
+    { code: 'HIS', name: 'Histoire' },
+    { code: 'JUN', name: 'Junior' },
+  ],
+  de: [
+    { code: 'ACT', name: 'Aktuelles & Gesellschaft' },
+    { code: 'CIN', name: 'Kino' },
+    { code: 'FIC', name: 'Fernsehfilme & Serien' },
+    { code: 'ART', name: 'Kunst & Kultur' },
+    { code: 'CUL', name: 'Popkultur & Alternativ' },
+    { code: 'DEC', name: 'Entdeckung' },
+    { code: 'HIS', name: 'Geschichte' },
+    { code: 'JUN', name: 'Junior' },
+  ]
 };
--- src/arteDB.js
+++ src/arteDB.js
@@ -1,144 +1,126 @@
-var http = require('http'),
-    request = require('request'),
-    q = require('q'),
-    arteCategories = require('./arteCategories'),
-    languages = ['fr','de'],
-    tmpTvGuideData = {};
+const request = require('request');
+const q = require('q');
+const arteCategories = require('./arteCategories');
 
-var updateTVGuidesInterval = setInterval(function(){
+const languages = ['fr', 'de'];
+const tmpTvGuideData = {};
 
-    updateTVGuides();
+setInterval(function () {
+  updateTVGuides();
 }, 360000);
 
 function updateTVGuides() {
+  console.log('Update TV Guides');
+  getAllShows().then(function () {
+    console.log('Updated');
+    return getAllCategories('fr');
+  }).then(function () {
+    return getAllCategories('de');
+  }).then(function () {
+    //clone the object, continue research for better implementation
+    module.exports.tvGuideData = JSON.parse(JSON.stringify(tmpTvGuideData));
 
-    console.log('Update TV Guides');
-    getAllShows().then( function() {
-
-        console.log('Updated');
-        return getAllCategories('fr');
-    }).then( function () {
-
-        return getAllCategories('de');
-    }).then( function () {
-
-        //clone the object, continue research for better implementation
-        module.exports.tvGuideData = JSON.parse(JSON.stringify(tmpTvGuideData));
-
-        module.exports.dataLoaded = true;
-        console.log('Categories updated');
-    })
+    module.exports.dataLoaded = true;
+    console.log('Categories updated');
+  });
 }
 
 function getAllShows() {
-    var deferredAll = [];
+  var deferredAll = [];
 
-    languages.map( function (language) {
-        var deferred = q.defer();
-        deferredAll.push(deferred.promise);
+  languages.forEach(function (language) {
+    var deferred = q.defer();
+    deferredAll.push(deferred.promise);
 
-        arteJSONCompiler('http://www.arte.tv/guide/' + language + '/plus7/videos?', 1, function(data) {
+    arteJSONCompiler(`http://www.arte.tv/guide/${language}/plus7/videos?`, 1, function (data) {
+      const newData = data.map(function (entry) {
+        // remove autoplay from arte show urls
+        entry.url = entry.url.replace(/=1/i, '=0');
+        entry.duration = (entry.duration / 60).toFixed(0);
+        entry.airdate_long = entry.scheduled_on;
+        entry.channels = '';
 
-            for (var i = 0, y = data.length; i < y; i++) {
+        return entry;
+      });
 
-                // remove autoplay from arte show urls
-                data[i].url = data[i].url.replace(/=1/i, '=0');
-                data[i].duration = (data[i].duration / 60).toFixed(0);
-                data[i].airdate_long = data[i].scheduled_on;
-                data[i].channels = '';
-            }
-
-            tmpTvGuideData[language] = data;
-            deferred.resolve();
-        });
-
+      tmpTvGuideData[language] = newData;
+      deferred.resolve();
     });
+  });
 
-    return q.all(deferredAll);
+  return q.all(deferredAll);
 }
 
 function getAllCategories(language) {
-    var deferredAll = [];
+  var deferredAll = [];
 
-    arteCategories.categories[language].map( function (category) {
-        var deferred = q.defer();
-        deferredAll.push(deferred.promise);
+  arteCategories.categories[language].forEach(function (category) {
+    var deferred = q.defer();
+    deferredAll.push(deferred.promise);
 
-        category.count = 0;
+    category.count = 0;
 
-        arteJSONCompiler('http://www.arte.tv/guide/' + language + '/plus7/videos?category=' + category.code + '&', 1, function(data) {
+    arteJSONCompiler(`http://www.arte.tv/guide/${language}/plus7/videos?category=${category.code}&`, 1, function (data) {
+      data.forEach(function (entry) {
+        addCategoryToShow(entry.id, category, language);
+      });
 
-            for (var i = 0, y = data.length; i < y; i++) {
-
-                addCategoryToShow(data[i].id, category, language);
-            }
-
-            deferred.resolve();
-        });
-
+      deferred.resolve();
     });
+  });
 
-    return q.all(deferredAll);
+  return q.all(deferredAll);
 }
 
 function addCategoryToShow(id, category, language) {
+  // do this for both languages
+  var show = getShowFromId(id, language);
 
-    // do this for both languages
-    var show = getShowFromId(id, language);
+  category.count++;
 
-    category.count++;
-
-    if (show) {
-        if (show.channels) {
-            show.channels+= ', ' + category.name;
-        }
-        else {
-            show.channels+= category.name;
-        }
+  if (show) {
+    if (show.channels) {
+      show.channels += `, ${category.name}`;
+    } else {
+      show.channels += category.name;
     }
-};
+  }
+}
 
 function getShowFromId(id, language) {
-
-    for (var i = 0, y = tmpTvGuideData[language].length; i < y; i++) {
-
-        if (tmpTvGuideData[language][i].id === id) {
-            return tmpTvGuideData[language][i];
-        }
+  for (let i = 0, y = tmpTvGuideData[language].length; i < y; i++) {
+    if (tmpTvGuideData[language][i].id === id) {
+      return tmpTvGuideData[language][i];
     }
+  }
 
-    return false;
+  return false;
 }
 
 function arteJSONCompiler(url, page, callback, tmpArray) {
-    tmpArray = tmpArray ? tmpArray : [];
+  tmpArray = tmpArray || [];
 
-    console.log('Request: ' + url + 'page=' + page + '&limit=72&sort=newest');
+  console.log(`Request: ${url}page=${page}&limit=72&sort=newest`);
 
-    request(url + 'page=' + page + '&limit=72&sort=newest', function (error, response, body) {
+  request(`${url}page=${page}&limit=72&sort=newest`, function (error, response, body) {
+    try {
+      const finalJson = JSON.parse(body);
+      const videos = finalJson.videos;
 
-        try {
-            var finalJson = JSON.parse(body),
-                videos = finalJson.videos;
+      tmpArray = tmpArray.concat(videos);
 
-            tmpArray = tmpArray.concat(videos);
-
-            if (finalJson.has_more) {
-
-                page++;
-                arteJSONCompiler(url, page, callback, tmpArray);
-            }
-            else {
-                console.log('Got JSON: ' + url + ' with ' + page + ' requests.');
-                callback(tmpArray);
-            }
-        }
-        catch (e){
-            console.log('request error: retry');
-            arteJSONCompiler(url, page, callback, tmpArray);
-        }
-
-    })
+      if (finalJson.has_more) {
+        page++;
+        arteJSONCompiler(url, page, callback, tmpArray);
+      } else {
+        console.log(`Got JSON: ${url} with ${page} requests.`);
+        callback(tmpArray);
+      }
+    } catch (e) {
+      console.log('request error: retry');
+      arteJSONCompiler(url, page, callback, tmpArray);
+    }
+  });
 }
 
 updateTVGuides();
