b09b96edb4a096c6935d320b4f45fbaa3a8f6e11
tobika <tobias.kausch@tobik.eu>
2017-05-04 17:24:33 +0200

    add test integration of rxjs data flow + prepare for new API

--- src/arteDB.js
+++ src/arteDB.js
@@ -1,138 +1,108 @@
 const request = require('request');
-const q = require('q');
 const arteCategories = require('./arteCategories');
 const moment = require('moment');
+const Rx = require('rxjs');
 
 const languages = ['fr', 'de'];
-const tmpTvGuideData = {};
 
 setInterval(function () {
   updateTVGuides();
 }, 360000);
 
+function createRequestDates() {
+  let date = new Date();
+  let dateArray = [];
+
+  for (let i = 0; i < 7; i++) {
+    dateArray.push(moment(date).format('YY/MM/DD'));
+    date.setDate(date.getDate() - 1);
+  }
+
+  return dateArray;
+}
+
 function updateTVGuides() {
-  console.log('Update TV Guides');
-  getAllShows().then(function () {
-    console.log('Updated');
-    return getAllCategories('fr');
-  }).then(function () {
-    return getAllCategories('de');
-  }).then(function () {
-    //clone the object, continue research for better implementation
-    module.exports.tvGuideData = JSON.parse(JSON.stringify(tmpTvGuideData));
+  const requestDates = createRequestDates();
+  let tmpTvGuideData = { fr: [], de: [] };
 
-    module.exports.dataLoaded = true;
-    console.log('Categories updated');
-  });
-}
-
-function getAllShows() {
-  var deferredAll = [];
-
-  languages.forEach(function (language) {
-    var deferred = q.defer();
-    deferredAll.push(deferred.promise);
-
-    arteJSONCompiler(`http://www.arte.tv/guide/${language}/plus7/videos?`, 1, function (data) {
-      const newData = data
-        .filter(function (entry) {
-          return moment(entry.scheduled_on).diff(moment(), 'days') > -7;
-        }).map(function (entry) {
-          // remove autoplay from arte show urls
-          entry.url = entry.url.replace(/=1/i, '=0');
-          entry.duration = (entry.duration / 60).toFixed(0);
-          entry.airdate_long = moment(entry.scheduled_on).format('DD/MM/YYYY');
-          entry.rights_end = moment(entry.rights_end).format('DD/MM/YYYY, hh:mm');
-          entry.channels = '';
-
-          if (entry.thumbnails[4]) {
-            entry.thumbnail_url = entry.thumbnails[4].url;
-          }
-
-          return entry;
+  Rx.Observable.from(languages)
+    .flatMap(function (language) {
+      return Rx.Observable.from(requestDates)
+        .concatMap(function (date) {
+          return test(date, language);
         });
-
-      tmpTvGuideData[language] = newData;
-      deferred.resolve();
-    });
-  });
-
-  return q.all(deferredAll);
-}
-
-function getAllCategories(language) {
-  var deferredAll = [];
-
-  arteCategories.categories[language].forEach(function (category) {
-    var deferred = q.defer();
-    deferredAll.push(deferred.promise);
-
-    category.count = 0;
-
-    arteJSONCompiler(`http://www.arte.tv/guide/${language}/plus7/videos?category=${category.code}&`, 1, function (data) {
-      data.filter(function (entry) {
-        return moment(entry.scheduled_on).diff(moment(),'days') > -7;
-      }).forEach(function (entry) {
-        addCategoryToShow(entry.id, category, language);
-      });
-
-      deferred.resolve();
-    });
-  });
-
-  return q.all(deferredAll);
-}
-
-function addCategoryToShow(id, category, language) {
-  // do this for both languages
-  var show = getShowFromId(id, language);
-
-  category.count++;
-
-  if (show) {
-    if (show.channels) {
-      show.channels += `, ${category.name}`;
-    } else {
-      show.channels += category.name;
-    }
-  }
-}
-
-function getShowFromId(id, language) {
-  for (let i = 0, y = tmpTvGuideData[language].length; i < y; i++) {
-    if (tmpTvGuideData[language][i].id === id) {
-      return tmpTvGuideData[language][i];
-    }
-  }
-
-  return false;
-}
-
-function arteJSONCompiler(url, page, callback, tmpArray) {
-  tmpArray = tmpArray || [];
-
-  console.log(`Request: ${url}page=${page}&limit=72&sort=newest`);
-
-  request({ url: `${url}page=${page}&limit=72&sort=newest`, timeout: 10000 }, function (error, response, body) {
-    try {
-      const finalJson = JSON.parse(body);
-      const videos = finalJson.videos;
-
-      tmpArray = tmpArray.concat(videos);
-
-      if (finalJson.has_more) {
-        page++;
-        arteJSONCompiler(url, page, callback, tmpArray);
-      } else {
-        console.log(`Got JSON: ${url} with ${page} requests.`);
-        callback(tmpArray);
+    })
+    .subscribe(
+      function (shows) {
+        tmpTvGuideData[shows.j].push(shows.i);
+        console.log('Next: ', shows);
+      },
+      function (err) {
+        console.log('Error: ', err);
+      },
+      function () {
+        console.log(tmpTvGuideData);
+        console.log('Completed');
       }
-    } catch (e) {
-      console.log('request error: retry');
-      arteJSONCompiler(url, page, callback, tmpArray);
-    }
+    );
+
+
+  // getShowsOfDate('17-05-03', 'fr').then(function (shows) {
+  //   console.log(shows);
+  //   module.exports.tvGuideData.fr = shows;
+  //   module.exports.dataLoaded = true;
+  // });
+}
+
+function test(i, j) {
+  const delay = Math.floor(Math.random() * 2000) + 500;
+
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve({ i, j });
+    }, delay);
   });
 }
+
+function getShowsOfDate(date, language) {
+  return new Promise(function (resolve, reject) {
+    request({ url: `http://www.arte.tv/guide/api/api/program/${language}/scheduled/${date}`, timeout: 10000 }, function (error, response, body) {
+      try {
+        const finalJson = JSON.parse(body);
+
+        const shows = finalJson
+          .filter(function (show) {
+            return show.playable;
+          })
+          .map(function (show) {
+            const showVideo = show.videos.filter(function (video) {
+              return video.kind === 'SHOW';
+            });
+
+            const rightsEnd = showVideo[0].videoRightsEnd;
+
+            return {
+              id: show.program.programId,
+              title: show.program.title,
+              duration: (show.durationRounded / 60).toFixed(0),
+              airdate_long: moment(show.broadcastBeginRounded).format('DD/MM/YYYY HH:mm'),
+              url: show.program.url,
+              rights_end: moment(rightsEnd).format('DD/MM/YYYY, hh:mm'),
+              thumbnail_url: show.program.mainImage.url,
+              subtitle: show.program.headerText,
+              teaser: show.program.teaserText,
+            };
+          });
+
+        resolve(shows.reverse());
+      } catch (e) {
+        console.log('request error', e);
+        reject(e);
+      }
+    });
+  });
+}
+
 
 updateTVGuides();
 
